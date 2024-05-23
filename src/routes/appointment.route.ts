3c6c791befea4c6d557ad01bfee5318ff7e7c48a
aimenabdulaziz <aimenabdulaziz@users.noreply.github.com>
2024-05-22 20:29:01 -0400

    chore: update linter and lint routes

--- src/routes/appointment.route.ts
+++ src/routes/appointment.route.ts
@@ -1,207 +1,207 @@
-import express from "express"
-import Appointment from "../models/appointment.model"
-import Patient from "../models/patient.model"
-import Practitioner from "../models/practitioner.model"
+import express from 'express';
+import Appointment from '../models/appointment.model';
+import Patient from '../models/patient.model';
+import Practitioner from '../models/practitioner.model';
 
 export default class AppointmentRouter {
-  public path = "/appointment"
-  public router = express.Router()
+  public path = '/appointment';
+  public router = express.Router();
 
   constructor() {
-    this.initializeRoutes()
-    console.log("Appointment router initialized...")
+    this.initializeRoutes();
+    console.log('Appointment router initialized...');
   }
 
   private initializeRoutes() {
     //get all appointments
-    this.router.get("/all", (req, res) => {
+    this.router.get('/all', (req, res) => {
       Appointment.model
         .find({})
         .then((docs) => {
-          return res.status(200).json(docs)
+          return res.status(200).json(docs);
         })
         .catch((err) => {
-          return res.status(500).json(err)
-        })
-    })
+          return res.status(500).json(err);
+        });
+    });
 
     // get by appointment ID
-    this.router.get("/id/:_id", (req, res) => {
+    this.router.get('/id/:_id', (req, res) => {
       Appointment.model
         .findById(req.params._id)
-        .populate("patient")
-        .populate("practitioner")
+        .populate('patient')
+        .populate('practitioner')
         .then((doc) => {
-          return res.status(200).json(doc)
+          return res.status(200).json(doc);
         })
         .catch((err) => {
-          return res.status(500).json(err)
-        })
-    })
+          return res.status(500).json(err);
+        });
+    });
 
     // create new appointment
-    this.router.post("/", (req, res) => {
+    this.router.post('/', (req, res) => {
       new Appointment.model(req.body)
         .save()
         .then((doc) => {
-          return res.status(200).json(doc)
+          return res.status(200).json(doc);
         })
         .catch((err) => {
-          return res.status(500).json(err)
-        })
-    })
+          return res.status(500).json(err);
+        });
+    });
 
     // create new appointment and tie to patient with given id
     this.router.post(
-      "/toPatient/:_id/toPractitioner/:pracId",
+      '/toPatient/:_id/toPractitioner/:pracId',
       async (req, res) => {
         // get specified practitioner
         const practitioner = await Practitioner.model.findById(
           req.params.pracId
-        )
+        );
         if (!practitioner) {
           return res
             .status(500)
-            .send("Could not find practitioner, appointment creation failed")
+            .send('Could not find practitioner, appointment creation failed');
         }
         // get specified patient
-        const patient = await Patient.model.findById(req.params._id)
+        const patient = await Patient.model.findById(req.params._id);
         if (!patient) {
           return res
             .status(500)
-            .send("Could not find patient, appointment creation failed")
+            .send('Could not find patient, appointment creation failed');
         }
         // create appointment
-        const appointment = new Appointment.model(req.body)
-        await appointment.save()
+        const appointment = new Appointment.model(req.body);
+        await appointment.save();
         if (!appointment) {
-          return res.status(500).send("Error, could not create appointment")
+          return res.status(500).send('Error, could not create appointment');
         }
 
         // update patient
-        appointment.patient = patient._id
-        patient.appointments.push(appointment._id)
-        patient.save()
+        appointment.patient = patient._id;
+        patient.appointments.push(appointment._id);
+        patient.save();
 
         // update practitioner
-        appointment.practitioner = practitioner._id
-        practitioner.appointments.push(appointment._id)
-        practitioner.save()
-        await appointment.save()
+        appointment.practitioner = practitioner._id;
+        practitioner.appointments.push(appointment._id);
+        practitioner.save();
+        await appointment.save();
 
-        return res.status(200).json(appointment)
+        return res.status(200).json(appointment);
       }
-    )
+    );
 
     // create multiple appointments and tie to patient and practitioner with given id
     this.router.post(
-      "/toPatient/:_id/toPractitioner/:pracId/multiple",
+      '/toPatient/:_id/toPractitioner/:pracId/multiple',
       async (req, res) => {
         // get and check practitioner
         const practitioner = await Practitioner.model.findById(
           req.params.pracId
-        )
+        );
         if (!practitioner) {
           return res
             .status(500)
-            .send("Error finding practitioner, appointments not created")
+            .send('Error finding practitioner, appointments not created');
         }
         // get and check patient
-        const patient = await Patient.model.findById(req.params._id)
+        const patient = await Patient.model.findById(req.params._id);
         if (!patient) {
           return res
             .status(500)
-            .send("Error finding patient, appointments not created")
+            .send('Error finding patient, appointments not created');
         }
 
         for (const appointment of req.body.appointments) {
-          const newAppointment = new Appointment.model(appointment)
-          newAppointment.patient = patient._id
-          newAppointment.practitioner = practitioner._id
-          newAppointment.save()
-          patient.appointments.push(newAppointment._id)
-          practitioner.appointments.push(newAppointment._id)
+          const newAppointment = new Appointment.model(appointment);
+          newAppointment.patient = patient._id;
+          newAppointment.practitioner = practitioner._id;
+          newAppointment.save();
+          patient.appointments.push(newAppointment._id);
+          practitioner.appointments.push(newAppointment._id);
         }
-        await patient.save()
-        await practitioner.save()
+        await patient.save();
+        await practitioner.save();
 
-        return res.status(200).json(patient.appointments)
+        return res.status(200).json(patient.appointments);
       }
-    )
+    );
 
     // create multiple appointments and tie to patient with given id
-    this.router.post("/toPatient/:_id/multiple", async (req, res) => {
+    this.router.post('/toPatient/:_id/multiple', async (req, res) => {
       // get and check patient
-      const patient = await Patient.model.findById(req.params._id)
+      const patient = await Patient.model.findById(req.params._id);
       if (!patient) {
         return res
           .status(500)
-          .send("Error finding patient, appointments not created")
+          .send('Error finding patient, appointments not created');
       }
 
       for (const appointment of req.body.appointments) {
-        const newAppointment = new Appointment.model(appointment)
-        newAppointment.patient = patient._id
-        newAppointment.save()
-        patient.appointments.push(newAppointment._id)
+        const newAppointment = new Appointment.model(appointment);
+        newAppointment.patient = patient._id;
+        newAppointment.save();
+        patient.appointments.push(newAppointment._id);
       }
-      await patient.save()
+      await patient.save();
 
-      return res.status(200).json(patient.appointments)
-    })
+      return res.status(200).json(patient.appointments);
+    });
 
     // update existing appointment by id
-    this.router.put("/:_id", async (req, res) => {
+    this.router.put('/:_id', async (req, res) => {
       const appointment = await Appointment.model.findByIdAndUpdate(
         req.params._id,
         req.body,
         { upsert: true }
-      )
+      );
       if (!appointment) {
-        return res.status(404).send("Error: could not find appointment")
+        return res.status(404).send('Error: could not find appointment');
       }
-      return res.status(200).json(req.body)
-    })
+      return res.status(200).json(req.body);
+    });
 
     // delete appointment
-    this.router.delete("/:_id", async (req, res) => {
-      const appointment = await Appointment.model.findById(req.params._id)
+    this.router.delete('/:_id', async (req, res) => {
+      const appointment = await Appointment.model.findById(req.params._id);
       if (!appointment)
-        return res.status(404).send("Error: could not find appointment")
+        return res.status(404).send('Error: could not find appointment');
 
       // find attached patient and practitioner
-      const patient = await Patient.model.findById(appointment.patient)
+      const patient = await Patient.model.findById(appointment.patient);
       if (!patient) {
         return res
           .status(404)
-          .send("Error: could not find patient for this appointment")
+          .send('Error: could not find patient for this appointment');
       }
       const practitioner = await Practitioner.model.findById(
         appointment.practitioner
-      )
+      );
       if (!practitioner) {
         return res
           .status(404)
-          .send("Error: could not find practitioner for this appointment")
+          .send('Error: could not find practitioner for this appointment');
       }
 
       // remove from attached patient
-      const patIdx = patient.appointments.indexOf(appointment._id)
-      patient.appointments.splice(patIdx, 1)
-      patient.save()
+      const patIdx = patient.appointments.indexOf(appointment._id);
+      patient.appointments.splice(patIdx, 1);
+      patient.save();
       // remove from attached practitioner
-      const pracIdx = practitioner.appointments.indexOf(appointment._id)
-      practitioner.appointments.splice(pracIdx, 1)
-      practitioner.save()
+      const pracIdx = practitioner.appointments.indexOf(appointment._id);
+      practitioner.appointments.splice(pracIdx, 1);
+      practitioner.save();
       // delete appointment from DB
       Appointment.model
         .deleteOne({ _id: appointment._id })
         .then(() => {
-          return res.status(200).send("Appointment deleted")
+          return res.status(200).send('Appointment deleted');
         })
         .catch((err) => {
-          return res.status(500).json(err)
-        })
-    })
+          return res.status(500).json(err);
+        });
+    });
   }
 }
--- src/routes/practitioner.route.ts
+++ src/routes/practitioner.route.ts
@@ -1,57 +1,57 @@
-import express from "express"
-import Practitioner from "../models/practitioner.model"
-import Appointment from "../models/appointment.model"
+import express from 'express';
+import Practitioner from '../models/practitioner.model';
+import Appointment from '../models/appointment.model';
 // import Patient from "../models/patient.model"
 
 export default class PractitionerRouter {
-  public path = "/practitioner"
-  public router = express.Router()
+  public path = '/practitioner';
+  public router = express.Router();
 
   constructor() {
-    this.initializeRoutes()
-    console.log("Practitioner router initialized...")
+    this.initializeRoutes();
+    console.log('Practitioner router initialized...');
   }
 
   private initializeRoutes() {
     // get all practitioners
-    this.router.get("/all", (req, res) => {
+    this.router.get('/all', (req, res) => {
       Practitioner.model
         .find({})
         .then((docs) => {
-          return res.status(200).json(docs)
+          return res.status(200).json(docs);
         })
         .catch((err) => {
-          return res.status(500).json(err)
-        })
-    })
+          return res.status(500).json(err);
+        });
+    });
     // get by ID
-    this.router.get("/id/:_id", async (req, res) => {
+    this.router.get('/id/:_id', async (req, res) => {
       const practitioner = await Practitioner.model
         .findOne({ _id: req.params._id })
-        .populate("patients")
-        .populate("appointments")
+        .populate('patients')
+        .populate('appointments');
       if (!practitioner) {
-        return res.status(404).json({ message: "Practitioner not found" })
+        return res.status(404).json({ message: 'Practitioner not found' });
       }
-      return res.status(200).json(practitioner)
-    })
+      return res.status(200).json(practitioner);
+    });
 
     // get by username
-    this.router.get("/user/:username", async (req, res) => {
+    this.router.get('/user/:username', async (req, res) => {
       const practitioner = await Practitioner.model
         .findOne({ username: req.params.username })
         .populate({
-          path: "patients",
+          path: 'patients',
           populate: {
-            path: "appointments",
-          },
+            path: 'appointments'
+          }
         })
-        .populate("appointments")
+        .populate('appointments');
       if (!practitioner) {
-        return res.status(404).json({ message: "Practitioner not found" })
+        return res.status(404).json({ message: 'Practitioner not found' });
       }
-      return res.status(200).json(practitioner)
-    })
+      return res.status(200).json(practitioner);
+    });
 
     // //temp route to assign practitioner to appointments
     // //commented out to avoid a deployed API fiasco, code remains because it's useful
@@ -89,102 +89,102 @@ export default class PractitionerRouter {
 
     // login route for demo docs
     // obviously not secure, but this is a demo
-    this.router.get("/login/:username/:password", async (req, res) => {
-      console.log("login attempt: ", req.params.username, req.params.password)
+    this.router.get('/login/:username/:password', async (req, res) => {
+      console.log('login attempt: ', req.params.username, req.params.password);
       const practitioner = await Practitioner.model.findOne({
-        username: req.params.username,
-      })
+        username: req.params.username
+      });
       if (!practitioner) {
-        return res.status(404).send("Practitioner not found")
+        return res.status(404).send('Practitioner not found');
       }
 
       if (practitioner.password !== req.params.password) {
-        return res.status(401).send("Incorrect password")
+        return res.status(401).send('Incorrect password');
       }
 
-      return res.status(200).json(practitioner)
-    })
+      return res.status(200).json(practitioner);
+    });
 
     // create new practitioner
-    this.router.post("/", async (req, res) => {
-      const practitioner = await new Practitioner.model(req.body).save()
+    this.router.post('/', async (req, res) => {
+      const practitioner = await new Practitioner.model(req.body).save();
       if (!practitioner) {
-        return res.status(500).send("Failed to create practitioner")
+        return res.status(500).send('Failed to create practitioner');
       }
-      return res.status(201).json(practitioner)
-    })
+      return res.status(201).json(practitioner);
+    });
 
     // update practitioner
-    this.router.put("/id/:_id", async (req, res) => {
+    this.router.put('/id/:_id', async (req, res) => {
       const practitioner = await Practitioner.model.findByIdAndUpdate(
         req.params._id,
         req.body,
         { new: true, upsert: true }
-      )
+      );
       if (!practitioner) {
         return res
           .status(500)
-          .json({ message: "Failed to update practitioner" })
+          .json({ message: 'Failed to update practitioner' });
       }
-      return res.status(200).json(practitioner)
-    })
+      return res.status(200).json(practitioner);
+    });
 
     // add patient
-    this.router.put("/id/:_id/addPatient/:patientId", async (req, res) => {
-      const practitioner = await Practitioner.model.findById(req.params._id)
-      const patient = await Practitioner.model.findById(req.params.patientId)
+    this.router.put('/id/:_id/addPatient/:patientId', async (req, res) => {
+      const practitioner = await Practitioner.model.findById(req.params._id);
+      const patient = await Practitioner.model.findById(req.params.patientId);
       if (practitioner && patient) {
-        practitioner.patients.push(patient._id)
-        practitioner.save()
+        practitioner.patients.push(patient._id);
+        practitioner.save();
       }
       if (!practitioner || !patient) {
         return res
           .status(500)
-          .json({ message: "Failed to add patient to practitioner" })
+          .json({ message: 'Failed to add patient to practitioner' });
       }
-      return res.status(200).json(practitioner)
-    })
+      return res.status(200).json(practitioner);
+    });
 
     // add appointment
     this.router.put(
-      "/id/:_id/addAppointment/:appointmentId",
+      '/id/:_id/addAppointment/:appointmentId',
       async (req, res) => {
-        const practitioner = await Practitioner.model.findById(req.params._id)
+        const practitioner = await Practitioner.model.findById(req.params._id);
         const appointment = await Practitioner.model.findById(
           req.params.appointmentId
-        )
+        );
         if (practitioner && appointment) {
-          practitioner.appointments.push(appointment._id)
-          practitioner.save()
+          practitioner.appointments.push(appointment._id);
+          practitioner.save();
         }
         if (!practitioner || !appointment) {
           return res
             .status(500)
-            .json({ message: "Failed to add appointment to practitioner" })
+            .json({ message: 'Failed to add appointment to practitioner' });
         }
-        return res.status(200).json(practitioner)
+        return res.status(200).json(practitioner);
       }
-    )
+    );
 
     // delete practitioner
-    this.router.delete("/id/:_id", async (req, res) => {
-      const practitioner = await Practitioner.model.findById(req.params._id)
+    this.router.delete('/id/:_id', async (req, res) => {
+      const practitioner = await Practitioner.model.findById(req.params._id);
       if (!practitioner) {
-        return res.status(404).send("Practitioner not found")
+        return res.status(404).send('Practitioner not found');
       }
       // remove from appointments
       const appointments = await Appointment.model.find({
-        _id: practitioner._id,
-      })
+        _id: practitioner._id
+      });
       appointments.forEach((appointment) => {
-        appointment.practitioner = null
-        appointment.save()
-      })
+        appointment.practitioner = null;
+        appointment.save();
+      });
 
       // delete practitioner
-      practitioner.deleteOne()
+      practitioner.deleteOne();
 
-      return res.status(200).send("Practitioner deleted")
-    })
+      return res.status(200).send('Practitioner deleted');
+    });
   }
 }
