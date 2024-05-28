1eedfb24531216e71a2dbfb9ef9be1ef2c052fb4
aimenabdulaziz <aimenabdulaziz@users.noreply.github.com>
2024-05-27 21:54:33 -0400

    feat: add routing for deleteUser and getAllUsers

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,12 +1,21 @@
 import { Router } from 'express';
-import { createUser, authenticateUser } from '../controllers/user.controller';
+import {
+  createUser,
+  authenticateUser,
+  getAllUsers,
+  deleteUser
+} from '../controllers/user.controller';
 
 const router = Router();
 
-// API:
+// API endpoints:
 // - /account/register
 // - /account/login
+// - /account/allUsers
+// - /account/deleteUser
 router.post('/register', createUser);
 router.post('/login', authenticateUser);
+router.get('/allUsers', getAllUsers);
+router.delete('/deleteUser', deleteUser);
 
 export default router;
