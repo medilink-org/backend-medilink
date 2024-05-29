45dddb2608e81d73b1c27d5cab375d3b224b20f9
aimenabdulaziz <aimenabdulaziz@users.noreply.github.com>
2024-05-29 15:43:42 -0400

    feat: add new router for sending an email

---
+++ src/routes/email.route.ts
@@ -0,0 +1,31 @@
+import sgMail from '@sendgrid/mail';
+import { Router } from 'express';
+
+const router = Router();
+
+router.post('/send-email', (req, res) => {
+  const { patientEmail, emailContent } = req.body;
+  console.log('Sending email to: ', patientEmail);
+  console.log('Email content: ', emailContent);
+
+  const msg = {
+    to: patientEmail,
+    from: 'aimen.25@dartmouth.edu',
+    subject: 'Visit Summary',
+    text: emailContent
+  };
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
+
+  sgMail
+    .send(msg)
+    .then(() => {
+      res.status(200).json({ message: 'Email sent successfully' });
+    })
+    .catch((error) => {
+      console.error('Error sending email:', error);
+      res.status(500).json({ message: 'Error sending email' });
+    });
+});
+
+export default router;
